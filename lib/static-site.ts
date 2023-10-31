fc76d15ac37291b7702530cb8b665506ef84b2a5
Eric Hendrickson <hendrickson02@gmail.com>
2023-10-31 22:59:46 +0000

    * Changed to using parent domain for object storage * Set up www redirect to parent domain * Added lambda function creation and API gateway to implementation

--- lib/static-site.ts
+++ lib/static-site.ts
@@ -12,11 +12,19 @@ import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import * as codepipelineActions from 'aws-cdk-lib/aws-codepipeline-actions';
 
+import { Architecture, Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as path from "path"
+
+import { Cors, LambdaIntegration, LogGroupLogDestination, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { LogGroup, RetentionDays } from "aws-cdk-lib/aws-logs";
+
 export interface StaticSiteProps {
   domainName: string;
-  siteSubDomain: string;
   owner: string;
   repo: string;
+  sesRegion?: string;
+  sesEmailAddress?: string;
 }
 
 /**
@@ -30,11 +38,11 @@ export class StaticSite extends Construct {
     super(parent, name);
 
     const zone = route53.HostedZone.fromLookup(this, 'Zone', { domainName: props.domainName });
-    const siteDomain = props.siteSubDomain + '.' + props.domainName;
-    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, 'cloudfront-OAI', {
-      comment: `OAI for ${name}`
+    const siteDomain = props.domainName;
+    const siteWithWWW = `www.${siteDomain}`;
+    const cloudfrontOai = new cloudfront.OriginAccessIdentity(this, 'CloudfrontOAI', {
+      comment: `OAI for ${name}: ${siteDomain}`
     });
-
     new CfnOutput(this, 'Site', { value: 'https://' + siteDomain });
 
     // Content bucket
@@ -48,27 +56,56 @@ export class StaticSite extends Construct {
        * the new bucket, and it will remain in your account until manually deleted. By setting the policy to
        * DESTROY, cdk destroy will attempt to delete the bucket, but will error if the bucket is not empty.
        */
-      removalPolicy: RemovalPolicy.DESTROY, // NOT recommended for production code
+      removalPolicy: RemovalPolicy.DESTROY,
 
       /**
        * For sample purposes only, if you create an S3 bucket then populate it, stack destruction fails.  This
        * setting will enable full cleanup of the demo.
        */
-      autoDeleteObjects: true, // NOT recommended for production code
+      autoDeleteObjects: true,
+    });
+
+    // WWW redirect bucket
+    const wwwRedirectBucket = new s3.Bucket(this, 'WWWRedirectSiteBucket', {
+      bucketName: siteWithWWW,
+      publicReadAccess: false,
+      // blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+
+      /**
+       * The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
+       * the new bucket, and it will remain in your account until manually deleted. By setting the policy to
+       * DESTROY, cdk destroy will attempt to delete the bucket, but will error if the bucket is not empty.
+       */
+      removalPolicy: RemovalPolicy.DESTROY,
+
+      // /**
+      //  * For sample purposes only, if you create an S3 bucket then populate it, stack destruction fails.  This
+      //  * setting will enable full cleanup of the demo.
+      //  */
+      // autoDeleteObjects: true,
+      websiteRedirect: { hostName: siteDomain },
     });
 
     // Grant access to cloudfront
     siteBucket.addToResourcePolicy(new iam.PolicyStatement({
       actions: ['s3:GetObject'],
       resources: [siteBucket.arnForObjects('*')],
-      principals: [new iam.CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+      principals: [new iam.CanonicalUserPrincipal(cloudfrontOai.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
     }));
     new CfnOutput(this, 'Bucket', { value: siteBucket.bucketName });
+
+    // Grant access to cloudfront
+    siteBucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [siteBucket.arnForObjects('*')],
+      principals: [new iam.CanonicalUserPrincipal(cloudfrontOai.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+    }));
+    new CfnOutput(this, 'WWWRedirectBucket', { value: wwwRedirectBucket.bucketName });
 
     // TLS certificate stack (note that a new stack must be created if not in us-east-1)
     const certificateStack = Stack.of(this).region == 'us-east-1' ?
       Stack.of(this)
-      : new Stack(this, 'CertificateStack', {
+      : new Stack(this, 'SiteCertificateStack', {
         env: {
           region: 'us-east-1',
           account: Stack.of(this).account,
@@ -78,11 +115,13 @@ export class StaticSite extends Construct {
     new CfnOutput(this, 'Stack', { value: certificateStack.stackName })
 
     // TLS certificate
+    const wildcardSiteDomain = `*.${siteDomain}`;
     const certificate = new acm.Certificate(certificateStack, 'Certificate', {
       domainName: siteDomain,
+      subjectAlternativeNames: [wildcardSiteDomain],
       validation: acm.CertificateValidation.fromDns(zone),
     });
-    new CfnOutput(this, 'Certificate', { value: certificate.certificateArn });
+    new CfnOutput(this, 'SiteCertificate', { value: certificate.certificateArn });
     
     // CloudFront distribution
     const distribution = new cloudfront.Distribution(this, 'SiteDistribution', {
@@ -99,21 +138,129 @@ export class StaticSite extends Construct {
         }
       ],
       defaultBehavior: {
-        origin: new cloudfront_origins.S3Origin(siteBucket, {originAccessIdentity: cloudfrontOAI}),
+        origin: new cloudfront_origins.S3Origin(siteBucket, { originAccessIdentity: cloudfrontOai }),
         compress: true,
         allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       }
-    })
-
+    });
     new CfnOutput(this, 'DistributionId', { value: distribution.distributionId });
 
-    // Route53 alias record for the CloudFront distribution
+    // CloudFront distribution for www redirect bucket
+    const wwwRedirectDistribution = new cloudfront.Distribution(this, 'WWWRedirectDistribution', {
+      certificate: certificate,
+      // defaultRootObject: "index.html",
+      domainNames: [siteWithWWW],
+      minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+      errorResponses:[
+        {
+          httpStatus: 403,
+          responseHttpStatus: 403,
+          responsePagePath: '/error.html',
+          ttl: Duration.minutes(30),
+        }
+      ],
+      defaultBehavior: {
+        origin: new cloudfront_origins.S3Origin(wwwRedirectBucket, { originAccessIdentity: cloudfrontOai }),
+        compress: true,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      }
+    });
+    new CfnOutput(this, 'WWWRedirectDistributionId', { value: wwwRedirectDistribution.distributionId });
+
+    // Route 53 alias record for the CloudFront distribution
     new route53.ARecord(this, 'SiteAliasRecord', {
       recordName: siteDomain,
       target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
       zone
     });
+
+    // Route 53 alias record for the CloudFront www redirect distribution
+    new route53.ARecord(this, 'WWWRedirectAliasRecord', {
+      recordName: siteWithWWW,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(wwwRedirectDistribution)),
+      zone
+    });
+
+    // AWS Lambda Contact Form Email Function
+    const sesResource = `arn:aws:ses:${props.sesRegion}:${Stack.of(this).account}:identity/${siteDomain}`;
+    const sesEmailAddress = String(props.sesEmailAddress);
+    const contactFormEmailFunction = new NodejsFunction(this, "ContactFormEmailFunction", {
+      architecture: Architecture.ARM_64,
+      runtime: Runtime.NODEJS_18_X,
+      entry: path.join(__dirname, "../lambdaFunctions/emailFunction.ts"),
+      environment: {
+        'SES_EMAIL_ADDRESS': sesEmailAddress
+      },
+      bundling: {
+        externalModules: [
+          '@aws-sdk/*', // Use the AWS SDK for JS v3 available in the Lambda runtime
+          'cool-module', // 'cool-module' is already available in a Layer
+        ],
+      },
+      logRetention: RetentionDays.ONE_DAY
+    });
+    contactFormEmailFunction.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          'ses:SendEmail',
+          'ses:SendRawEmail',
+          'ses:SendTemplatedEmail',
+        ],
+        resources: [
+          sesResource
+        ],
+      })
+    )
+    new CfnOutput(this, 'EmailFunction', { value: contactFormEmailFunction.functionName });
+
+    // API Gateway certificate
+    const apiDomainName = `api.${siteDomain}`;
+    const apiGatewayCertificate = new acm.Certificate(this, 'ApiGatewayCertificate', {
+      domainName: apiDomainName,
+      validation: acm.CertificateValidation.fromDns(zone),
+    });
+    new CfnOutput(this, 'ApiCertificate', { value: apiGatewayCertificate.certificateArn });
+
+    const restApiName = `${this.toString()}-staticPageApi`;
+    const logGroupName = `${this.toString()}_static_page_api`
+    const api = new RestApi(this, "ApiGateway", {
+      cloudWatchRole: true,
+      domainName: {
+        domainName: apiDomainName,
+        certificate: apiGatewayCertificate
+      },
+      restApiName,
+      defaultCorsPreflightOptions: {
+        allowHeaders: Cors.DEFAULT_HEADERS,
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS
+      },
+      deployOptions: {
+        accessLogDestination: new LogGroupLogDestination(new LogGroup(this, "ApiLogGroup", {
+            logGroupName,
+            retention: RetentionDays.ONE_DAY,
+            removalPolicy: RemovalPolicy.DESTROY
+        }))
+      }
+    });
+
+    // Add record for API
+    new route53.ARecord(this, 'CustomDomainAliasRecord', {
+      recordName: apiDomainName,
+      zone,
+      target: route53.RecordTarget.fromAlias(new targets.ApiGateway(api))
+    });
+    new CfnOutput(this, "Api", { value: api.restApiName });
+    new CfnOutput(this, "ApiAliasUrl", { value: `https://${apiDomainName}` });
+
+    // Add Lambda function to API
+    const contactFormResourceName = "contact";
+    api.root
+      .addResource(contactFormResourceName)
+      .addMethod('POST', new LambdaIntegration(contactFormEmailFunction));
 
     // AWS CodePipeline pipeline
     const pipeline = new codepipeline.Pipeline(this, "SitePipeline", {
@@ -138,6 +285,7 @@ export class StaticSite extends Construct {
       }
     });
 
+    // Source
     const sourceOutput = new codepipeline.Artifact();
     const sourceAction = new codepipelineActions.GitHubSourceAction({
       actionName: 'GithubSource',
@@ -163,6 +311,7 @@ export class StaticSite extends Construct {
       actions: [buildAction],
     });
 
+    // Deploy
     const deployAction = new codepipelineActions.S3DeployAction({
       actionName: 'S3Deploy',
       bucket: siteBucket,
@@ -172,7 +321,6 @@ export class StaticSite extends Construct {
       stageName: 'Deploy',
       actions: [deployAction]
     });
-
     new CfnOutput(this, 'Pipeline', { value: pipeline.pipelineName });
   }
 }
